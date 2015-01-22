d209299688bae6979b6c71ae90393eba02dd61e6
maxapryg <m.prytkova@sup.com>
2015-01-22 14:58:26 +0400

    Merge origin/serg
    Conflicts:
    	src/test/java/com/livejournal/uitests/utility/RandomText.java

--- src/main/java/com/livejournal/uisteps/thucydides/Databases.java
+++ src/main/java/com/livejournal/uisteps/thucydides/Databases.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import org.junit.Assert;
 
 /**
  *
@@ -14,52 +15,70 @@ import java.util.List;
  */
 public class Databases {
 
+    Connection c = null;//Соединение с БД
+    List<ArrayList<String>> answers = new ArrayList<ArrayList<String>>();
+
     public BaseConnect workWithDB() {
+        answers.clear();
         return new BaseConnect();
     }
 
     public class BaseConnect {
 
-        public void conect(String base) {
+        public BaseSelect conect() {
 
-            String user = "root";//Логин пользователя
-            String password = "";//Пароль пользователя
-            String url = "jdbc:mysql://127.0.0.1:2222/" + base;//URL адрес
             String driver = "com.mysql.jdbc.Driver";//Имя драйвера
-            ArrayList<String> answer = new ArrayList<>();
 
             try {
                 Class.forName(driver);//Регистрируем драйвер
             } catch (ClassNotFoundException e) {
+
                 e.printStackTrace();
+                Assert.assertTrue("No connection", false);
             }
-
-            Connection c = null;//Соединение с БД
-
-            try {
-                c = (Connection) DriverManager.getConnection(url, user, password);//Установка соединения с БД
-                System.out.println("================ соединение есть");
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                //Обязательно необходимо закрыть соединение
-                try {
-                    if (c != null) {
-                        c.close();
-                        System.out.println("================ соединение закрыто");
-                    }
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-
+            return new BaseSelect();
         }
 
     }
 
     public class BaseSelect {
 
+        public BaseSelect select(String select, String column) {
+
+            String user = "root";//Логин пользователя
+            String password = "";//Пароль пользователя
+            String url = "jdbc:mysql://127.0.0.1:2222/livejournal";//URL адрес
+            ArrayList<String> answer = new ArrayList<>();
+
+            try {
+                c = (Connection) DriverManager.getConnection(url, user, password);//Установка соединения с БД
+                ThucydidesUtils.putToSession("connect", c);
+                Statement st = c.createStatement();//Готовим запрос
+                ResultSet rs = st.executeQuery(select);//Выполняем запрос к БД, результат в переменной rs
+                while (rs.next()) {
+                    answer.add(rs.getString(column));
+                    // System.out.println(rs.getString(column));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                Assert.assertTrue("No connection", false);
+            }
+            answers.add(answer);
+            return new BaseSelect();
+        }
+
+        public List<ArrayList<String>> finish() {
+            //Обязательно необходимо закрыть соединение
+            try {
+                if (c != null) {
+                    c.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+                Assert.assertTrue("No connection", false);
+            }
+            return answers;
+        }
     }
 
 }
